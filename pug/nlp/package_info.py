fccbb18014a6514157eeaf8299609ab56ab69184
Hobson Lane <hobsonlane@gmail.com>
2015-03-17 14:06:48 -0700

    bump

--- pug/nlp/package_info.py
+++ pug/nlp/package_info.py
@@ -6,7 +6,7 @@ __namespace_package__ = 'pug'
 __subpackage__ = 'nlp'
 __doc__ = "{}.{} -- Natural Language Processing (NLP) python utilities by and for the PDX Python User Group (PUG)".format(__namespace_package__, __subpackage__)
 __url__ = "https://github.com/hobson/{}-{}".format(__namespace_package__, __subpackage__)
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 __author__ = "Hobson <hobson@totalgood.com>"
 __authors__ = (
     "Hobson <hobson@totalgood.com>",
