48cf3c5ee9bb5dfd2c3c44a11f49a75a505b4ca8
Hobson Lane <hobsonlane@gmail.com>
2015-03-25 15:51:59 -0700

    discrete pug.nlp.env.get()

--- pug/nlp/env.py
+++ pug/nlp/env.py
@@ -3,7 +3,7 @@
 import sys
 import os
 
-def get(var_name, default=False):
+def get(var_name, default=False, verbosity=0):
     """ Get the environment variable or assume a default, but let the user know about the error."""
     try:
         value = os.environ[var_name]
@@ -11,8 +11,10 @@ def get(var_name, default=False):
             return None
         return value
     except:
-        from traceback import format_exc
-        msg = "Unable to find the %s environment variable.\nUsing the value %s (the default) instead.\n" % (var_name, default)
-        sys.stderr.write(format_exc())
-        sys.stderr.write(msg)
+        if verbosity >= 0:
+            msg = "Unable to find the %s environment variable.\nUsing the value %s (the default) instead.\n" % (var_name, default)
+            if verbosity > 0:
+                from traceback import format_exc
+                sys.stderr.write(format_exc())
+            sys.stderr.write(msg)
         return default
