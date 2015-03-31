718dab4dfdce6a0a49c0b13c55ca3f42189952db
Hobson Lane <hobsonlane@gmail.com>
2015-03-30 21:20:11 -0700

    dont install from __url__

--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ setup(
     #tests_require = ['django-setuptest', 'south'],
     #test_suite = 'setuptest.setuptest.SetupTestSuite',
     #cmdclass = {'test': test},
-    url = __url__,
+    # url = __url__,
 
     # Force setup.py to use the latest github master source files rather than the cheeseshop tarball: 
     download_url = "{}/tarball/master".format(__url__),
