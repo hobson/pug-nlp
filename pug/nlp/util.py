521e44022e8e028ee9cd8c7c3865177c96eac924
Hobson Lane <hobsonlane@gmail.com>
2015-04-01 20:45:28 -0700

    mutch ado about nothing (fuzzy_get_value is not that different from fuzzy_get)

--- pug/nlp/util.py
+++ pug/nlp/util.py
@@ -776,16 +776,16 @@ def fuzzy_get_value(obj, approximate_key, default=None, **kwargs):
       dict_keys (list of str): if you already have a set of keys to search, this will save this funciton a little time and RAM
 
     Examples:
-      >>> fuzzy_get_value({'seller': 2.7, 'sailor': set('e')}, 'sail')
-      set(['e'])
+      >>> fuzzy_get_value({'seller': 2.7, 'sailor': set('e')}, 'sail') == set(['e'])
+      True
       >>> fuzzy_get_value({'seller': 2.7, 'sailor': set('e'), 'camera': object()}, 'SLR')
       2.7
-      >>> fuzzy_get_value({'seller': 2.7, 'sailor': set('e'), 'camera': object()}, 'I')
-      set(['e'])
-      >>> fuzzy_get_value({'word': tuple('word'), 'noun': tuple('noun')}, 'woh!', similarity=.3, key_and_value=True)
-      ('word', ('w', 'o', 'r', 'd'))
+      >>> fuzzy_get_value({'seller': 2.7, 'sailor': set('e'), 'camera': object()}, 'I') == set(['e'])
+      True
+      >>> fuzzy_get_value({'word': tuple('word'), 'noun': tuple('noun')}, 'woh!', similarity=.3)
+      ('w', 'o', 'r', 'd')
       >>> df = pd.DataFrame(np.arange(6*2).reshape(2,6), columns=('alpha','beta','omega','begin','life','end'))
-      >>> fuzzy_get(df, 'life')[0], fuzzy_get(df, 'omega')[0]
+      >>> fuzzy_get_value(df, 'life')[0], fuzzy_get(df, 'omega')[0]
       4, 2
     """
     dict_obj = OrderedDict(obj)
@@ -793,9 +793,8 @@ def fuzzy_get_value(obj, approximate_key, default=None, **kwargs):
         return dict_obj[dict_obj.keys()[int(approximate_key)]]
     except (ValueError, IndexError):
         pass
-    k = fuzzy_get(dict_obj, approximate_key, **kwargs)
-    return dict_obj.get(k, default)
-    
+    return fuzzy_get(dict_obj, approximate_key, key_and_value=False, **kwargs)
+
 
 def fuzzy_get_tuple(dict_obj, approximate_key, dict_keys=None, key_and_value=False, similarity=0.6, default=None):
     """Find the closest matching key and/or value in a dictionary (must have all string keys!)"""
