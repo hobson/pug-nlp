7c5974e828e9bcc545ace12eef7ed09f4bb621db
Hobson Lane <hobsonlane@gmail.com>
2015-04-03 17:14:53 -0700

    250 tests and generate_files along with find_files

--- pug/nlp/util.py
+++ pug/nlp/util.py
@@ -3414,10 +3414,12 @@ def path_status(path, filename='', status=None, verbosity=0):
 
 
 def find_files(path='', ext='', level=None, typ=list, dirs=False, files=True, verbosity=0):
-    """Recursively find all files in the indicated directory with the indicated file name extension
+    """ Recursively find all files in the indicated directory
+
+    Filter by the indicated file name extension (ext)
 
     Args:
-      path (str):
+      path (str):  Root/base path to search.
       ext (str):   File name extension. Only file paths that ".endswith()" this string will be returned
       level (int, optional): Depth of file tree to halt recursion at. 
         None = full recursion to as deep as it goes
@@ -3446,21 +3448,74 @@ def find_files(path='', ext='', level=None, typ=list, dirs=False, files=True, ve
       >>> (d for d in find_files(os.path.dirname(__file__), ext='.py') if d['name'] == 'util.py').next()['size'] > 1000
       True
 
+      There should be an __init__ file in the same directory as this script.
+      And it should be at the top of the list.
+      >>> sorted(d['name'] for d in generate_files(os.path.dirname(__file__), ext='.py', level=0))[0]
+      '__init__.py'
+      >>> sorted(generate_files().next().keys())
+      ['accessed', 'created', 'dir', 'mode', 'modified', 'name', 'path', 'size', 'type']
+      >>> all(d['type'] in ('file','dir','symlink->file','symlink->dir','mount-point->file','mount-point->dir','block-device','symlink->broken','pipe','special','socket','unknown') for d in generate_files(level=1, files=True, dirs=True))
+      True
+    """
+    it = iter(generate_files(path, ext=ext, level=level, dirs=dirs, files=files, verbosity=verbosity))
+    if isinstance(typ(), collections.Mapping):
+        return typ((ff['path'], ff) for ff in it)
+    elif typ is not None:
+        return typ(it)
+    else:
+        return it
+
+
+def generate_files(path='', ext='', level=None, dirs=False, files=True, verbosity=0):
+    """ Recursively generate files (and thier stats) in the indicated directory 
+    
+    Filter by the indicated file name extension (ext)
+
+    Args:
+      path (str):  Root/base path to search.
+      ext (str):   File name extension. Only file paths that ".endswith()" this string will be returned
+      level (int, optional): Depth of file tree to halt recursion at. 
+        None = full recursion to as deep as it goes
+        0 = nonrecursive, just provide a list of files at the root level of the tree
+        1 = one level of depth deeper in the tree
+      typ (type):  output type (default: list). if a mapping type is provided the keys will be the full paths (unique)
+      dirs (bool):  Whether to yield dir paths along with file paths (default: False)
+      files (bool): Whether to yield file paths (default: True)
+        `dirs=True`, `files=False` is equivalent to `ls -d`
+
+    Returns: 
+      list of dicts: dict keys are { 'path', 'name', 'bytes', 'created', 'modified', 'accessed', 'permissions' }
+        path (str): Full, absolute paths to file beneath the indicated directory and ending with `ext`
+        name (str): File name only (everythin after the last slash in the path)
+        size (int): File size in bytes
+        created (datetime): File creation timestamp from file system
+        modified (datetime): File modification timestamp from file system
+        accessed (datetime): File access timestamp from file system
+        permissions (int): File permissions bytes as a chown-style integer with a maximum of 4 digits 
+        type (str): One of 'file', 'dir', 'symlink->file', 'symlink->dir', 'symlink->broken'
+          e.g.: 777 or 1755
+
+    Examples:
+      >>> 'util.py' in [d['name'] for d in find_files(os.path.dirname(__file__), ext='.py', level=0)]
+      True
+      >>> (d for d in find_files(os.path.dirname(__file__), ext='.py') if d['name'] == 'util.py').next()['size'] > 1000
+      True
 
       There should be an __init__ file in the same directory as this script.
       And it should be at the top of the list.
       >>> sorted(d['name'] for d in find_files(os.path.dirname(__file__), ext='.py', level=0))[0]
       '__init__.py'
+      >>> os.path.join(os.path.dirname(__file__), '__init__.py') in find_files(
+      ... os.path.dirname(__file__), ext='.py', level=0, typ=dict)
+      True
       >>> sorted(find_files()[0].keys())
       ['accessed', 'created', 'dir', 'mode', 'modified', 'name', 'path', 'size', 'type']
-      >>> all(d['type'] in ('file','dir','symlink->file','symlink->dir','mount-point->file','mount-point->dir','block-device','symlink->broken','pipe','special','socket','unknown') for d in find_files(level=1, files=True, dirs=True))
+      >>> all(d['type'] in ('file','dir','symlink->file','symlink->dir','mount-point->file','mount-point->dir','block-device','symlink->broken','pipe','special','socket','unknown')
+      ... for d in find_files(level=1, files=True, dirs=True))
       True
     """
     path = path or './'
-    if isinstance(typ(), collections.Mapping):
-        return typ((ff['path'], ff) for ff in find_files(path, ext=ext, level=level, typ=None, dirs=dirs, files=files, verbosity=verbosity))
-    elif typ is not None:
-        return typ(find_files(path, ext=ext, level=level, typ=None, dirs=dirs, files=files, verbosity=verbosity))
+    ext = str(ext).lower()
 
     for dir_path, dir_names, filenames in walk_level(path, level=level):
         if verbosity > 0:
