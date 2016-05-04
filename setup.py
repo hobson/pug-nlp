bf339da84e55bdfd22386e8514ba4dd67f46e87f
Hobson Lane <hobson@talentpair.com>
2016-05-04 11:14:28 -0700

    rename pug/nlp to pug_nlp to match namespace package convention in PyScaffold

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-    Setup file for nlp.
+    Setup file for pug_nlp (pug-nlp).
 
     This file was generated with PyScaffold 2.5.5, a tool that easily
     puts up a scaffold for your new Python project. Learn more under:
@@ -16,6 +16,7 @@ def setup_package():
     needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
     sphinx = ['sphinx'] if needs_sphinx else []
     setup(setup_requires=['six', 'setuptools>=20.3.1', 'pyscaffold>=2.5,<2.6a0'] + sphinx,
+          tests_require=['pytest_cov', 'pytest'],
           use_pyscaffold=True)
 
 
