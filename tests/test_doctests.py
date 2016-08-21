27e75b3629feda4ff7156d27a5cc2fe731cd679e
Hobson Lane <github@totalgood.com>
2016-08-21 13:40:57 -0700

    manually test a few

---
+++ tests/test_doctests.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+"""
+Uses the python unittest module to test this app with `python -m unittest pug.nlp`.
+"""
+
+# from django.test import TestCase
+from unittest import TestCase, main
+import doctest
+from pug.nlp import util, http, regex, penn_treebank_tokenizer, detector_morse
+
+
+class NLPDocTest(TestCase):
+
+    def test_module(self, module=None):
+        if module:
+            failure_count, test_count = doctest.testmod(
+                module,
+                optionflags=doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE,
+                raise_on_error=False,
+                verbose=True)
+            msg = "Ran {0} tests in {3} and {1} passed ({2} failed)".format(test_count, test_count-failure_count, failure_count, module.__file__)
+            print msg
+            if failure_count:
+                # print "Ignoring {0} doctest failures...".format(__file__)
+                self.fail(msg)
+            # return failure_count, test_count
+
+    def test_util(self):
+        self.test_module(util)
+
+    def test_http(self):
+        self.test_module(http)
+
+    def test_regex_patterns(self):
+        self.test_module(regex)
+
+    def test_penn_treebank_tokenizer(self):
+        self.test_module(penn_treebank_tokenizer)
+
+    def test_detector_morse(self):
+        self.test_module(detector_morse)
+
+
+if __name__ == '__main__':
+    main()
